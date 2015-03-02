3b523d77d8b7b566c59d0eb9df87229847ea52e4
salazaru <uri.salazar@outlook.com>
2015-03-02 10:26:01 -0800

    Signed-off-by: salazaru <uri.salazar@outlook.com>

--- Hmwk/Assignment_1/Savitch_9thEd_Chap1_ProgProj_Prob4/main.cpp
+++ Hmwk/Assignment_1/Savitch_9thEd_Chap1_ProgProj_Prob4/main.cpp
@@ -1,19 +1,42 @@
 /* 
  * File:   main.cpp
- * Author: rcc
- *
+ * Author: Mr. Uriel Salazar
  * Created on March 2, 2015, 9:03 AM
+ *      Purpose: Free Fall problem from Savitch
  */
 
-#include <cstdlib>
+//System Libraries
+#include <iostream>
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+//User Libraries
 
+//Global Constants
+const float G=3.2174e1f;//Acceleration in ft/sec^2 on Earth
+const float CNVFTMT=3.048e-1f;//Conversion from feet to 
+
+//Function Prototypes
+
+//Execution Begins Here!!!
+int main(int argc, char** argv) {
+    //Declare Variables
+    float time,dstnce;
+    //Prompt for time
+    cout<<"Free Fall Problem"<<endl;
+    cout<<"Input the time to drop x.xx (seconds)"<<endl;
+    cin>>time;
+    //Calculate Distance
+    dstnce=static_cast<float>(1)/2*G*time*time;
+    //dstnce=1/2*G*time*time;//Does not work!!!!!!!!
+    //dstnce=1.0f/2*G*time*time;
+    //dstnce=1/2.0f*G*time*time;
+    //dstnce=G*1/2*time*time;
+    //dstnce=0.5f*G*time*time;
+    //dstnce=5e-1f*G*time*time;
+    cout<<"Distance Fallen = "<<dstnce<<"(feet)"<<endl;
+    cout<<"Distance Fallen = "<<dstnce*CNVFTMT<<"(feet)"<<endl;
+    //Exit Stage Right!
     return 0;
 }
 

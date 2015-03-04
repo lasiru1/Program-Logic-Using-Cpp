5232a125575cc93bcab02d1647ea56817f002e05
salazaru <uri.salazar@outlook.com>
2015-03-04 14:03:16 -0800

    Signed-off-by: salazaru <uri.salazar@outlook.com>

--- Hmwk/Assignment_1/Gadis_7thEd_Chap2_Prob_2/nbproject/Makefile-variables.mk
+++ Hmwk/Assignment_1/Gadis_7thEd_Chap2_Prob_2/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
 CND_ARTIFACT_NAME_Debug=gadis_7thed_chap2_prob_2
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/gadis_7thed_chap2_prob_2
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/gadis_7thed_chap2_prob_2
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
 CND_PACKAGE_NAME_Debug=gadis7thedchap2prob2.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/gadis7thedchap2prob2.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/gadis7thedchap2prob2.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
 CND_ARTIFACT_NAME_Release=gadis_7thed_chap2_prob_2
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/gadis_7thed_chap2_prob_2
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/gadis_7thed_chap2_prob_2
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
 CND_PACKAGE_NAME_Release=gadis7thedchap2prob2.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/gadis7thedchap2prob2.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/gadis7thedchap2prob2.tar
 #
 # include compiler specific variables
 #

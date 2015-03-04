19e3a353f3efce21183be3e73bf3a705edf06c96
salazaru <uri.salazar@outlook.com>
2015-03-04 10:51:47 -0800

    Signed-off-by: salazaru <uri.salazar@outlook.com>

---
+++ Lab/BigLetterC/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ * Created on March 4, 2015, 9:35 AM
+ *      Purpose: Demonstration
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+unsigned char CNVPCT=100;
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    char c;//character to be used
+    //Prompt for the letter C to be user
+    cout<<"What letter would you like to use for Big"
+    cin>>c;
+    // Output Big C
+    cout<<"   "<<c<<c<<c<<c<<endl;
+    cout<<"   "<<c<<c<<c<<c<<endl;
+    cout<<"   "<<c<<c<<c<<c<<endl;
+    cout<<"   "<<c<<c<<c<<c<<endl;
+    cout<<"   "<<c<<c<<c<<c<<endl;
+    cout<<"   "<<c<<c<<c<<c<<endl;
+    //Output Everything
+    
+    return 0;
+}

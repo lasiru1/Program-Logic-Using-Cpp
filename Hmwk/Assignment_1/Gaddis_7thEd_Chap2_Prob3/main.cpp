2f691539367bd6b5162b3a23d5eb4645d5cfcc74
salazaru <uri.salazar@outlook.com>
2015-03-04 23:51:23 -0800

    Signed-off-by: salazaru <uri.salazar@outlook.com>

---
+++ Hmwk/Assignment_1/Gaddis_7thEd_Chap2_Prob3/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.cpp
+ * Author: Uriel Salazar
+ * Created: March 4, 2015, 7:51 PM
+ * Purpose: 3rd Homework Assignment 
+ */
+
+//System Libraries
+#include <cstdlib>//I/O Library
+using namespace std;//I/O Library under Standard Namespace
+
+//User Libraries
+
+//Global Constants
+unsigned char CNVPCNT=100;
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    float prchse, sTax, cTax, tTax;
+    unsigned char 
+    
+    
+    //Exit Stage Right!
+    return 0;
+}

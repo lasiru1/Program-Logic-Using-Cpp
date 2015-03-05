5311a7c2bcc30792845ff697abc91f68ef8c8838
salazaru <uri.salazar@outlook.com>
2015-03-05 14:51:35 -0800

    Signed-off-by: salazaru <uri.salazar@outlook.com>

--- Hmwk/Assignment_1/Gaddis_7thEd_Chap2_Prob1/main.cpp
+++ Hmwk/Assignment_1/Gaddis_7thEd_Chap2_Prob1/main.cpp
@@ -1,19 +1,35 @@
 /* 
  * File:   main.cpp
- * Author: Uri
- *
- * Created on March 5, 2015, 2:01 PM
+ * Author: Uriel Salazar
+ * Created: March 5, 2015, 2:10 PM
+ * Purpose: 5th Homework Problem (Sum of Two Numbers)
  */
 
-#include <cstdlib>
+//System Libraries
+#include <iostream>//I/O Library
+using namespace std;//I/O Library under Standard Namespace
 
-using namespace std;
+//User Libraries
 
-/*
- * 
- */
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
 int main(int argc, char** argv) {
-
+    //Declare Variables
+    short fVal, sVal, total;
+    //Input Assigned Values
+    fVal=6.2e1;//First Value
+    sVal=9.9e1;//Second Value
+    total=fVal+sVal;//The Sum of the two Values
+    //Output Everything
+    cout<<"The Sum of Two Numbers"<<endl;
+    cout<<"Known/Given Values:"<<endl;
+    cout<<"First Value = "<<fVal<<endl;
+    cout<<"Second Value = "<<sVal<<endl;
+    cout<<"Calculated Values:"<<endl;
+    cout<<"The Sum of the Two Values = "<<total<<endl;
+    //Exit Stage Right!
     return 0;
 }
-

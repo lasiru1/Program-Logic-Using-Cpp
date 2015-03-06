8e8af715ded6d0bf4df6b48839e8731d220d32f1
salazaru <uri.salazar@outlook.com>
2015-03-05 18:08:03 -0800

    Big Letter C Demonstration

--- Lab/BigLetterC/main.cpp
+++ Lab/BigLetterC/main.cpp
@@ -1,8 +1,8 @@
 /* 
  * File:   main.cpp
- * Author: rcc
- * Created on March 4, 2015, 9:35 AM
- *      Purpose: Demonstration
+ * Author: Dr. Mark E. Lehr
+ * Created on March 4, 2015, 8:22 AM
+ *      Purpose:  Demonstration
  */
 
 //System Libraries
@@ -12,25 +12,27 @@ using namespace std;
 //User Libraries
 
 //Global Constants
-unsigned char CNVPCT=100;
 
 //Function Prototypes
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
-    //Declare Variables
-    char c;//character to be used
+    //Declare variables
+    char c;//Character to be used to display C
     //Prompt for the letter C to be user
-    cout<<"What letter would you like to use for Big"
+    cout<<"What letter would you like to use for Big C"<<endl;
     cin>>c;
-    // Output Big C
+    //Output Big C
     cout<<"   "<<c<<c<<c<<c<<endl;
+    cout<<"  "<<c<<"    "<<c<<endl;
+    cout<<" "<<c<<"      "<<c<<endl;
+    cout<<c<<endl;
+    cout<<c<<endl;
+    cout<<c<<endl;
+    cout<<c<<endl;
+    cout<<" "<<c<<"      "<<c<<endl;
+    cout<<"  "<<c<<"    "<<c<<endl;
     cout<<"   "<<c<<c<<c<<c<<endl;
-    cout<<"   "<<c<<c<<c<<c<<endl;
-    cout<<"   "<<c<<c<<c<<c<<endl;
-    cout<<"   "<<c<<c<<c<<c<<endl;
-    cout<<"   "<<c<<c<<c<<c<<endl;
-    //Output Everything
-    
+    //Exit stage right
     return 0;
 }
--- Lab/BigLetterC/nbproject/Makefile-variables.mk
+++ Lab/BigLetterC/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
 CND_ARTIFACT_NAME_Debug=bigletterc
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/bigletterc
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/bigletterc
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
 CND_PACKAGE_NAME_Debug=bigletterc.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/bigletterc.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/bigletterc.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
 CND_ARTIFACT_NAME_Release=bigletterc
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/bigletterc
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/bigletterc
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
 CND_PACKAGE_NAME_Release=bigletterc.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/bigletterc.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/bigletterc.tar
 #
 # include compiler specific variables
 #

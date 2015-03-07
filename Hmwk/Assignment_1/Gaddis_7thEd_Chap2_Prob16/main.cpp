fd92bfcfeaf39610210e2f428f9088e96ba64ee1
salazaru <uri.salazar@outlook.com>
2015-03-07 12:54:56 -0800

    Large CS w/ FlowChart

---
+++ Hmwk/Assignment_1/Gaddis_7thEd_Chap2_Prob16/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Uriel Salazar
+ * Created: March 7, 2015, 11:22 AM
+ * Purpose: 9th Homework Problem (Diamond Pattern)
+ */
+
+//System Libraries
+#include <iostream>//I/O Library
+using namespace std;//I/O Library under Standard Namespace
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+
+    //Output Diamond
+    cout<<"   *"<<endl;
+    cout<<"  ***"<<endl;
+    cout<<" *****"<<endl;
+    cout<<"*******"<<endl;
+    cout<<" *****"<<endl;
+    cout<<"  ***"<<endl;   
+    cout<<"   *"<<endl;   
+    //Exit Stage Right!
+    return 0;
+}
---
+++ Hmwk/Assignment_1/Savitch_9thEd_Chap1_ProgProj_Prob2/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Uriel Salazar
+ * Created: March 7, 2015, 12:01 PM
+ * Purpose: 10th Homework Problem (Large CS)
+ */
+
+//System Libraries
+#include <iostream>//I/O Library
+using namespace std;//I/O Library under Standard Namespace
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+
+    //Output Diamond
+    cout<<"**************************************************"<<endl;
+    cout<<""<<endl;
+    cout<<"          C C C           S S S S        !!       "<<endl;
+    cout<<"        C       C       S         S      !!       "<<endl;
+    cout<<"       C               S                 !!       "<<endl;
+    cout<<"      C                 S                !!       "<<endl;
+    cout<<"      C                  S S S S         !!       "<<endl;
+    cout<<"      C                           S      !!       "<<endl;
+    cout<<"       C                           S     !!       "<<endl;
+    cout<<"        C       C       S         S               "<<endl;
+    cout<<"          C C C          S S S S         00       "<<endl;
+    cout<<""<<endl;
+    cout<<"**************************************************"<<endl;
+    cout<<"        Computer Science is Cool Stuff!!!         "<<endl;
+    //Exit Stage Right!
+    return 0;
+}

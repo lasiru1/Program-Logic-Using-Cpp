cdf33f4a65a17c1b05019802f1186581f404045e
salazaru <uri.salazar@outlook.com>
2015-03-08 23:30:53 -0700

    Cyborg Data Type Sizes w/ FlowChart

---
+++ Hmwk/Assignment_1/Gaddis_7thEd_Chap2_Prob9/main.cpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   main.cpp
+ * Author: Uriel Salazar
+ * Created: March 8, 2015, 11:15 PM
+ * Purpose: Homework Problem (Cyborg Data Type Sizes)
+ */
+
+//System Libraries
+#include <iostream>//I/O Library
+using namespace std;//I/O Library under Standard Namespace
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Output Data Type Sizes
+    cout<<"The size of a character is "<<sizeof(char)<<" byte.\n";
+    cout<<"The size of an integer is "<<sizeof(int)<<" bytes.\n";
+    cout<<"The size of a float is "<<sizeof(float)<<" bytes.\n";
+    cout<<"The size of a double is "<<sizeof(double)<<" bytes.\n";
+    //Exit Stage Right!
+    return 0;
+}

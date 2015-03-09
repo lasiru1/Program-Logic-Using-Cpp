fe48c7bf590496e04a85377672a84261f3d44f54
salazaru <uri.salazar@outlook.com>
2015-03-08 22:48:59 -0700

    Personal Information w/ FlowChart

---
+++ Hmwk/Assignment_1/Gaddis_7thEd_Chap2_Prob14/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Uriel Salazar
+ * Created: March 8, 2015, 10:33 PM
+ * Purpose: Homework Problem (Personal Information)
+ */
+
+//System Libraries
+#include <iostream>//I/O Library
+using namespace std;//I/O Library under Standard Namespace
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    
+    //Output Personal Information
+    cout<<"Uriel Salazar\n"
+        <<"634 E Cornell Dr., Rialto, CA, 92376\n"
+        <<"1-909-202-9661\n"
+        <<"Computer Science & Engineering\n";
+    //Exit Stage Right!
+    return 0;
+}

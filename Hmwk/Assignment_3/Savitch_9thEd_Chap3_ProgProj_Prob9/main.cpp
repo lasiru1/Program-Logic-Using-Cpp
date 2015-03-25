63ff4a2c7cb745199f89cbd6f7300d0715923039
salazaru <uri.salazar@outlook.com>
2015-03-25 10:10:47 -0700

    Monty Hall Game Show Problem v4

--- Hmwk/Assignment_3/Savitch_9thEd_Chap3_ProgProj_Prob9/main.cpp
+++ Hmwk/Assignment_3/Savitch_9thEd_Chap3_ProgProj_Prob9/main.cpp
@@ -15,8 +15,6 @@ using namespace std;    //I/O Library under Standard Namespace
 //User Libraries
 
 //Global Constants
-const unsigned short CNVPCNT = 1e2;
-
 
 //Function Prototypes
 
@@ -24,13 +22,16 @@ const unsigned short CNVPCNT = 1e2;
 int main(int argc, char** argv)
 {
     //Declare Variables
-    int car, cPrize1, cPrize2;
+    int car, cnPrize;
     int choiceA, choiceB;
+    int change;
     int tallyA, tallyB;
 
     //Seed the random number generator
     srand(time(0));
-        
+    
+    //Initialize variables
+    tallyA = 0, tallyB = 0;
     
     //Simulate the program 10,000 times
     for (int smulatn = 1; smulatn <= 10000; smulatn++)
@@ -40,60 +41,49 @@ int main(int argc, char** argv)
        
         //Randomly select location for the Car
         car = (rand() % 3) + 1;
-        
-        cPrize1 = (rand() % 3) + 1;
-        cPrize2 = (rand() % 3) + 1;
-        
+ 
         //Make the revealed door a consolation prize
-        //cPrize1 cannot be the same number as the car or choiceA
-        while(cPrize1 == car || cPrize1 == choiceA)
-        {
-            cPrize1 = (rand() % 3) + 1;
-        }
-
-        //Make the reamaing door a consolation prize
-        //cPrize2 cannot be the same number as cPrize1 or choiceA
-        while(cPrize2 == cPrize1 || cPrize2 == choiceA)
-        {
-            cPrize2 = (rand() % 3) + 1;
-        }
-
-        //Randomly choose to stick with the origial choice or switch choices
-        //choiceB = 1 or 2, 1 = stay, 2 = switch
-        choiceB = (rand() % 2) + 1;
+        //cnPrize cannot be the same number as the car or choiceA
+        do
+        {    
+            cnPrize = (rand() % 3) + 1;
+        } while (cnPrize == car || cnPrize == choiceA);
         
-        //Determine if sticking with the original choice wins the car
-        if (choiceB == 1)
+        //Make the remaining door a possible car
+        //choiceB cannot be the same number as the consolation prize of choiceA
+        do
+        {    
+            choiceB = (rand() % 3) + 1;
+        } while (choiceB == cnPrize || choiceB == choiceA);
+        
+        //Randomly choose to stick with the origial choice or switch choices
+        //choiceB = 1 or 2. 1 -> stay, else, -> switch.
+        change = (rand() % 2) + 1;
+        
+        //Determine whether sticking with the original choice wins the car
+        //or whether switching choices wins the car
+        if (change == 1)
         {
             if (choiceA == car)
             {
                 tallyA++;
             }
         }
-        //Determine if switching choices wins the car
         else
         {
-            choiceA == cPrize2;
-            if (choiceA == car)
+            choiceA = choiceB;
+            if (choiceB == car)
             {
                 tallyB++;
             }
         }
     }
-        
+
     //Ouput the Results
     cout << tallyA << endl;
-    cout << tallyB << endl;
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    cout << tallyB << endl;  
+
+
     //Exit Stage Right!
     return 0;
 }

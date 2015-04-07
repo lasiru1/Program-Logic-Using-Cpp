0ee752e71d2385d0e2b80f2b66593f16d1b6ac1b
salazaru <uri.salazar@outlook.com>
2015-04-07 00:46:10 -0700

    Update

--- Hmwk/Assignment_3/Assignment_3/nbproject/Makefile-impl.mk
+++ Hmwk/Assignment_3/Assignment_3/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Assignment_3_Menu
+PROJECTNAME=Assignment_3
 
 # Active Configuration
 DEFAULTCONF=Debug
--- Hmwk/Assignment_3/Assignment_3/nbproject/Makefile-variables.mk
+++ Hmwk/Assignment_3/Assignment_3/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
-CND_ARTIFACT_NAME_Debug=assignment_3_menu
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/assignment_3_menu
+CND_ARTIFACT_NAME_Debug=assignment_3
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/assignment_3
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
-CND_PACKAGE_NAME_Debug=assignment3menu.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/assignment3menu.tar
+CND_PACKAGE_NAME_Debug=assignment3.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/assignment3.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
-CND_ARTIFACT_NAME_Release=assignment_3_menu
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/assignment_3_menu
+CND_ARTIFACT_NAME_Release=assignment_3
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/assignment_3
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
-CND_PACKAGE_NAME_Release=assignment3menu.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/assignment3menu.tar
+CND_PACKAGE_NAME_Release=assignment3.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/assignment3.tar
 #
 # include compiler specific variables
 #

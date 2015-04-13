f62d0194a7a120625dcb7ee077a63bc1eddec310
salazaru <uri.salazar@outlook.com>
2015-04-13 16:44:21 -0700

    Pattern Displays

---
+++ Book/Assignment_4/Gaddis_8thEd_Chap5_Prob23/main.cpp
@@ -0,0 +1,48 @@
+//File: main.cpp
+//==========================================================================
+//Programmer: Uriel Salazar
+//==========================================================================
+//Created: April 13, 2015, 3:42 PM
+//==========================================================================
+//Purpose: Pattern Displays
+//==========================================================================
+
+//System Libraries
+#include <iostream>     //Input/Output Library
+#include <iomanip>      //Parametric Library
+using namespace std;    //Input/Output Library under standard name space
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins
+int main()
+{
+    //Pattern A
+    for (int i = 1; i <= 10; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "+";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    
+    //Pattern B
+    for (int i = 10; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "+";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    //Exit Program
+    return 0;
+}

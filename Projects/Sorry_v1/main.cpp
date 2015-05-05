d3477769b8fb16dc0eadb8a53a0db071341ca04b
salazaru <uri.salazar@outlook.com>
2015-05-05 13:36:04 -0700

    Sorry! v1

---
+++ Projects/Sorry_v1/main.cpp
@@ -0,0 +1,31 @@
+//File: main.cpp
+//==========================================================================
+//Programmer: Uriel Salazar
+//==========================================================================
+//Created: May 4, 2015, 6:11 PM
+//==========================================================================
+//Purpose: Project 1 - "Sorry"
+//==========================================================================
+
+//System Libraries
+#include <iostream>     //Input/Output Library
+#include <iomanip>      //Parametric Library
+using namespace std;    //Input/Output Library under standard name space
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins
+int main()
+{
+    //Declare variables
+    
+
+
+
+    //Exit the program
+    return 0;
+}
